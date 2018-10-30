1f3060154995c1875f19352874fa8bee647737df
Matthieu Poullet <matthieu.poullet@gmail.com>
2018-10-30 10:52:29 +0100

    Fix language spelling.

--- tests/integration_en_us.sh
+++ tests/integration_en_us.sh
@@ -4,8 +4,8 @@ set -u
 set -o pipefail
 set -x
 
-# Tests that the command "how do you say hi in spanish"
+# Tests that the command "How do you say hi in spanish?"
 #   will return "Hola" to verify the Assistant SDK
 #   end-to-end.
-echo "how do you say hi in spanish" | ./run_assistant_text \
+echo "How do you say hi in spanish?" | ./run_assistant_text \
   --credentials ./credentials.json --verbose | grep "Hola"
--- tests/integration_fr.sh
+++ tests/integration_fr.sh
@@ -4,9 +4,9 @@ set -u
 set -o pipefail
 set -x
 
-# Tests that the command "how do you say hello in spanish"
-#   will return "buenos dias" to verify the Assistant SDK
+# Tests that the command "How do you say hello in spanish?"
+#   will return "Buenos dias" to verify the Assistant SDK
 #   end-to-end.
-echo "comment dit-on bonjour en español" | ./run_assistant_text \
+echo "Comment dit-on bonjour en espagnol?" | ./run_assistant_text \
   --credentials ./credentials.json \
   --locale "fr-FR" --verbose | grep "Buenos dias"
