4353a5111ac83dd64515eaa39f9a7210aa23fc91
Nick Felker <fleker@google.com>
2018-11-15 12:58:30 -0800

    custom build script
    Change-Id: I6c8e2b5385899e1cfb13fa38b3cdc5b895d9bb89

---
+++ tests/build-cb.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+set -x
+PROJECT_PATH=$(pwd)
+
+# Step 0. Install git?
+apt-get update
+apt-get install -y git
+
+# Step 1. Clean up any dependencies
+echo "Cleaning dependencies"
+./tests/clean-all.sh
+
+# Step 2. Install dependencies
+echo "Installing dependencies"
+apt-get install -y autoconf automake libtool build-essential curl unzip pkg-config
+apt-get install -y libasound2-dev  # For ALSA sound output
+apt-get install -y libcurl4-openssl-dev # CURL development library
+
+# Step 3. Build protocol buffer, gRPC, and Google APIs
+git clone -b "$(curl -L https://grpc.io/release)" https://github.com/grpc/grpc
+GRPC_PATH=${PROJECT_PATH}/grpc
+cd "${GRPC_PATH}"
+# Checkout stable release of gRPC
+git checkout v1.15.0
+git submodule update --init
+
+echo "Compiling gRPC protobufs"
+cd third_party/protobuf
+./autogen.sh && ./configure && make
+make install
+ldconfig
+
+echo "Compiling gRPC"
+cd "${GRPC_PATH}"
+make clean
+LDFLAGS="-lm" make
+make install
+ldconfig
+
+echo "Compiling Google APIs"
+cd "${PROJECT_PATH}"
+git clone https://github.com/googleapis/googleapis.git
+cd googleapis/
+make LANGUAGE=cpp
+
+# Step 4. Build assistant-grpc
+echo "Compiling C++ Assistant"
+cd "${PROJECT_PATH}"
+GOOGLEAPIS_GENS_PATH=${PROJECT_PATH}/googleapis/gens make run_assistant
+
+# If we get to this point, the project has successfully been built, and we can exit.
+echo "Project built!"
